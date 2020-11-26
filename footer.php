faa882f6982b6885d0b11bfcad156304a904d565
Asia Altharhani <74899479+AsiaAltharhani@users.noreply.github.com>
2020-11-26 20:20:50 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,75 @@
+
+
+<style>
+  .menueFooter {
+    float: right;
+    margin-right: 15px;
+  }
+  .menueFooter a {
+
+    color: black;
+    float: left;
+  }
+  .menueFooter ul li {
+    color: black;
+  }
+  .footer .address{
+    float: left;
+    color: black;
+    margin-top: 25px;
+  }
+
+  .address img{
+    text-align: center;
+  }
+  .vl{
+    border-left: 1px dashed salmon;
+     height: 80px;
+     position: absolute;
+     left: 50%;
+     margin-left: -3px;
+     margin-top:20px;
+     margin-bottom:30px;
+     top: 0;
+  }
+
+  #CopyF{
+    text-align: center;
+    color:black;
+    padding-top:80px;
+    padding-left:45%;
+    padding-right: 45%;
+  }
+
+</style>
+</body>
+
+    <div class="footer" style="">
+
+      <address class="address">
+    Contact us: <a  href="mailto:petcat134@gmail.com" style="color:Salmon;">petcat134@gmail.com</a><br>
+
+    <br><p style="float:left;">Follow us on Instagram:
+    <p> <a href="#"> <img src="style/icon.png" alt="iconInstegram" width="23" height="23"></a></p>  </p>
+    <br>
+    </address>
+
+    <div class="menueFooter">
+      <ul>
+        <li><a href="index.php">Home </a></li>
+        <li><a href="aboutUsPage.php">About us </a></li>
+        <li><a href="BreedsCats.php">Breeds </a></li>
+        <li> <a href="Behavior.php"> Behaviors </a></li>
+        <li> <a href="care&health.php"> Care&Health</a> </li>
+        <li> <a href="products.php"> Products </a></li>
+      </ul>
+    </div>
+
+    <div class="vl"></div>
+
+    <br>
+    <p id="CopyF"> © 2020 Pet Cat</p>
+
+</div>
+
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="shourtcut icon" href="style/logo12.ico">
+    <link rel="stylesheet" href="style/style.css">
+
+
+
+  </head>
+  <body>
+
+
+<!--navigation top-->
+<div class="navbar">
+  <a id="xx" href="index.php"><img src="style/logo11.png" alt="logo" width="120" height="45"></a>
+
+  <a href="products.php"> Products </a>
+  <a href="care&Health.php"> Care&Health </a>
+  <a href="Behaviors.php"> Behaviors</a>
+  <a href="BreedsCats.php"> Breeds </a>
+  <a href="aboutUsPage.php"> About us </a>
+  <a class="active" href="index.php">Home</a>
+</div>
