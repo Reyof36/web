c6b47846d1f5efdce0a76fbcc38203ca0544935d
Asia Altharhani <74899479+AsiaAltharhani@users.noreply.github.com>
2020-11-26 20:18:19 +0300

    Add files via upload

---
+++ Behaviors.php
@@ -0,0 +1,59 @@
+<?php require_once 'style/header.php';?>
+<title> Pet Cat | Behaviors & information about your Cat  </title>
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<div class="container-flex">
+
+  <ul class="breedcrumb">
+    <li> <a href="index.php"> Home </a> </li>
+    <li>  Behaviors </li>
+  </ul>
+
+<div class="row">
+<div class="column">
+  <div class="card">
+    <img src="style/attachedCat.jpg" alt="Attached Cat" id="imgCard" >
+    <h3> Why Is My Cat So Attached To Me?</h3>
+  <p> <a href="post1Behavior.php" style="color: black;"> Read More >  </a> </p>
+  </div>
+</div>
+
+<div class="column">
+  <div class="card">
+    <img src="style/catBite.jpg" alt="cat Bite" id="imgCard" >
+    <h3> Why Does My Cat Bite Me Gently?</h3>
+  <p> <a href="post2Behavior.php" style="color: black;"> Read More >  </a> </p>
+  </div>
+</div>
+
+<div class="column">
+  <div class="card">
+    <img src="style/dp2.jpg" alt="cat" id="imgCard" >
+    <h3> How To Stop My Cat From Bullying My Other Cat?</h3>
+  <p> <a href="post3Behaviors.php" style="color: black;"> Read More  </a> </p>
+  </div>
+</div>
+
+<div class="column">
+  <div class="card">
+    <img src="style/pc4.jpg" alt="cat" id="imgCard" >
+      <h3> XXXXXXXXXXXXXX</h3>
+    <p> <a href="aboutUsPage.php" style="color: black;"> Read More  </a> </p>
+  </div>
+</div>
+
+</div>
+</div>
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<?php require_once 'style/footer.php';?>
---
+++ aboutUsPage.php
@@ -0,0 +1,77 @@
+<?php require_once 'style/header.php';?>
+
+<title> Pet Cat | About us </title>
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<style >
+  .breedcrumb {
+    position: absolute;
+    top: 8px;
+    left: 16px;
+  }
+.container-flexAbout .center {
+    position:absolute;
+    top: 6%;
+    margin-left: 40px;
+    margin-right: 40px;
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+  .center p{
+    padding-left: 25px;
+    padding-right: 25px;
+    margin-left:60px;
+    margin-right: 60px;
+    margin-top: 5px;
+    font-size: 18px;
+    font-family:cursive;
+    color: black;
+  }
+
+
+
+</style>
+
+<div class="container-flexAbout" style=" margin-left: auto; margin-right: auto; position:relative; width:80%; text-align: center;">
+  <img src="style/about.jpg" alt="cat" style="width:100%; opacity:.6;">
+
+  <div class="breedcrumb" style="border:none;">
+    <li> <a href="index.php"> Home </a> </li>
+    <li> About </li>
+  </ul>
+  </div>
+  <div class="center">
+    <h2> Welcome To The world of Pet Cat</h2><br>
+
+        <p> We have spent literally thousands of hours to share our cat experience and expertise with you.<br></p>
+        <h3>Our mission: </h3>
+        <p>Is to create a reliable source of information about cat care and behavior.<br> We never sell our opinion.
+           It means that we’ll never recommend any product or service for money. <br>We research the information online
+           and combine this knowledge with our experience  to provide a quality insight that would be helpful for
+           cat owners around the world.</p><br>
+
+          <!-- <div class="blockSocial">
+              <h5 style="color:black;">Our Soical Media Account:</h5>
+              <a href="#"> <img src="style/icon.png" alt="icon" width="20" height="20"> </a><br>
+              To Contact us:<a href="mailto:petcat134@gmail.com">petcat134@gmail.com</a>
+          </div> -->
+
+
+  </div>
+
+</div>
+
+  <br>
+  <br>
+  <hr>
+  <br>
+  <br>
+<?php require_once 'style/footer.php';?>
+<!--
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+</script>-->
