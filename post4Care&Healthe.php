530904680ebd5cb9bef09b1d5d128a46a81e4ef9
Esraa <75171813+eman-2@users.noreply.github.com>
2020-11-28 17:44:04 +0300

    Add files via upload

---
+++ post4Care&Healthe.php
@@ -0,0 +1,131 @@
+<?php require_once 'style/header.php';?>
+    <title> Pet Cat |  What are the types of cat food? </title>
+<br>
+<br>
+<hr>
+<br>
+<br>
+<style>
+  .container-flex h2{
+    margin-left:40px;
+  }
+  .container-flex img{
+    margin: 40px;
+    margin-top: 15px;
+  }
+  .container-flex p {
+    margin-left: 40px;
+    margin-right: 30px;
+  }
+  .container-flex h4{
+    margin-left: 50px;
+  }
+  .container-flex ul {
+      margin-left: 50px;
+      margin-right: 30px;
+    }
+</style>
+
+<div class="container-flex">
+  <!--Add breedcrumb-->
+  <ul class="breedcrumb">
+    <li> <a href="index.php"> Home </a> </li>
+    <li> <a href="care&health.php"> Care&Health</a> </li>
+    <li> What are the types of cat food?</li>
+  </ul>
+
+  <br>
+  <br>
+  <h2>What are the types of cat food?</h2>
+  <img src="style/food cat.jpeg" alt="Food Cat" width="700" height="400">
+  <p> <p>There is a chasm between cat owners who feed their pets raw or homemade cat food and veterinarians who warn that without quality control,
+       the risks of bacterial contamination or nutritional deficiency are too high.
+    <br>If you choose to go it alone, be warned: Making your own cat food is an exacting and time-consuming business. 
+    Striking the right balance of ingredients, including vitamins and minerals, and properly storing the food are critical for a happy and healthy animal.</br></p>
+
+      <br>
+      <h4>What Do Cats Need to Eat?</h4>
+      <p>As obligate carnivores, cats need:
+          <ul>
+              <li>Protein from meat or fish</li>
+              <li>Amino acids like taurine and arginine (from meat or fish)</li>
+              <li>Fatty acids</li>
+              <li>Vitamins</li>
+              <li>Minerals</li>
+              <li>Water</li>
+          </ul> </p>
+          <p>Carbohydrates like rice and corn in small amounts are fine, but they aren’t necessary for a cat’s diet. However, a modest amount of carbs will provide useful energy and may reduce the cost of a home cooked diet,
+               says Rebecca Remillard, a board-certified veterinary nutritionist who works with the MSPCA Angell Animal Medical Center in Boston and maintains a dietary consulting business.
+            <br> 
+            Nutrition guidelines for cats are available through the American Association of Feed Control Officials.</br></p>
+      
+      
+               <br>
+      <h4>Raw Cat Food: The Risks and the Rewards.</h4>
+      <p>Remillard says raw meat diets are neither safe nor nutritionally sound. She’s backed up by the American Animal Hospital Association,
+           which warns against the risk of salmonella poisoning to both the cat and human members of the household.
+       
+        
+         <br>“There are a lot of people who want to feed raw. I tell them they have to be aware of zoonotic (animal to human) disease transmission, food safety, and contagion issues.
+         I don’t think veterinarians should get upset about it, but they should make clear the health issues,” Remillard says.</br>
+         <br>Feeding cats a raw meat diet also leaves too much room for variables, another reason she doesn’t advocate such a diet.
+          The potential for slip-up is high if the owner goes out of town and the food is left out too long or if they substitute one ingredient for another, she says.</br>
+          <br>Lisa Pierson, a veterinarian in Lomita, Calif., is familiar with the arguments and disputes them passionately on her own web site.
+           She says she hasn’t had issues with bacterial contamination in the six years she has made her own cat food because she is careful:
+            She knows where the meat comes from, she parboils mostly rabbit and bone-in chicken, grinds it herself, and adds minerals like taurine to make sure her cats are eating a balanced diet.</br>
+            <br>She says it’s also cheaper than higher-quality canned food and would take an owner of two cats about two hours a month to make.</br>
+    </p>
+      <br>
+      <h4>Raw Meat Diet: A Recipe for Healthy Cats</h4>
+ 
+      <p>The following recipe was developed by Pierson. It yields enough food for 10-14 days for the average cat.
+          <ul>
+              <li>3 pounds of whole fowl or rabbit, including bones, organs, and skin.</li>
+              <li>1 cup water.</li>
+              <li>400 IU vitamin E (powdered E in capsule form works).</li>
+              <li>100 mg vitamin B-complex (start with a smaller amount when beginning a raw meat diet; the vitamin has a strong odor).</li>
+              <li>2000 mg taurine, powdered.</li>
+              <li>¾ tsp lite salt with iodine (when using chicken parts).</li>
+              <li>Liver (add 4 oz if the meat you are using does not include organs).</li>
+              <li>Psyllium (add when first introducing the raw meat diet to your cat).</li>
+          
+          </ul> </p>
+      <br>
+      <h4>Cooked Diet: A Recipe for Healthy Cats</h4>
+      <p>The following recipe, from the MSPCA-Angell Animal Medical Center, is for a day’s feeding of an average 12-pound adult cat with no medical problems.
+           It is endorsed by Remillard.
+
+        Use a dietary gram scale to weigh out foods until you get accustomed to the correct measurements, and make sure all ingredients are well blended before serving. 
+        The food must be kept refrigerated or frozen between meals, and for palatability’s sake, warmed before being given to your cat.
+    <br>Mix together:</br>
+<ul>
+    <li>Protein: cooked dark meat chicken, beef, pork, lamb, salmon, or tuna (83 grams or 3 wt-ounces).</li>
+    <li>Carbohydrate: cooked white rice, oatmeal, barley, corn, peas, or pasta (50 grams or 1/3 cup).</li>
+    <li>Fiber: cooked sweet potato, without skin (30 grams or 1/5 cup)</li>
+    <li>Fat (optional): vegetable, safflower, olive oil or fish oil (1/4 teaspoon).</li>
+    <li>Balance IT Feline: 2.7 grams or half a red scoop (in the container). This is a commercial blend of vitamins and minerals.</li>
+
+
+</ul></p>
+     
+    <br>
+  </p>
+
+  <br>
+  <br>
+  <!--add comment form-->
+  <div class="comment">
+   <h3 style="text-align:center; color:gray;"> Leave a comment </h3>
+   <br>
+      <?php require_once 'comment.php';?>
+  </div>
+  <br>
+  <br>
+</div>
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+<?php require_once 'style/footer.php';?>
