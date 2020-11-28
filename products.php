b474f0dc8ab0015e0b037e5d35d1e04bb43ef769
ReyofAltasan <47700757+ReyofAltasan@users.noreply.github.com>
2020-11-28 04:31:00 +0300

    Add files via upload

--- products.php
+++ products.php
@@ -2,6 +2,7 @@
 
 <title> Pet Cat | Products </title>
 
+
 <br>
 <br>
 <hr>
@@ -13,11 +14,86 @@
      <li> <a href="index.php"> Home </a> </li>
      <li> Products </li>
    </ul>
+ <h1 style="font-size:17px; text-align:center;">These are some of the sites that sell products for your cat  </h1>
+ <br>
+
+<div class="row">
+<div class="column">
+  <div class="card">
+    <img src="style/18.jpg" alt="cat" id="imgCard" >
+    <h3>  Cat Bed </h3>
+  <p> <a href="https://www.amazon.com.au/Calming-Comfortable-Cuddler-Washable-Waterproof/dp/B083KJ45S4/ref=pd_vtp_199_5/356-2141964-9712054?_encoding=UTF8&pd_rd_i=B083KJ45S4&pd_rd_r=82d7f755-cdbf-4928-9633-fe37959babd6&pd_rd_w=TwwQi&pd_rd_wg=UH8GR&pf_rd_p=0b01ce65-5ac7-489e-80cc-43c8dabd96e8&pf_rd_r=T49S4GMG8YSS9G8GQHCY&psc=1&refRID=T49S4GMG8YSS9G8GQHCY" style="color: black;"> Click here   </a> </p>
+  </div>
+</div>
+
+<div class="column">
+  <div class="card">
+    <img src="style/11.jpg" alt="cat" id="imgCard" >
+    <h3> Dry Food  </h3>
+  <p> <a href="https://www.amazon.com.au/Hills-Science-Diet-Optimal-Chicken/dp/B07GBP8DBB/ref=sr_1_2?dchild=1&keywords=cat+food&qid=1606495954&sr=8-2" style="color: black;"> Click here   </a> </p>
+  </div>
+</div>
+
+<div class="column">
+  <div class="card">
+    <img src="style/48.jpg" alt="cat" id="imgCard" >
+    <h3>Cat Toys </h3>
+  <p> <a href="https://www.amazon.com.au/Chiwava-Rattle-Kitten-Interactive-Assorted/dp/B01KNTBIEE/ref=sr_1_21?crid=EPAH9ZDMPAT7&dchild=1&keywords=cat+toys&qid=1606496651&s=pet-supplies&sprefix=ca%2Cpets%2C616&sr=1-21" style="color: black;"> Click here </a> </p>
+  </div>
+</div>
 
 
- </div>
- <br>
- <br>
- <hr>
- <br>
- <?php require_once 'style/footer.php';?>
+
+<div class="column">
+  <div class="card">
+    <img src="style/56.jpg" alt="cat" id="imgCard" >
+    <h3> Cat Bowl  </h3>
+  <p> <a href="https://www.amazon.com.au/dp/B07VLY53LK/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B07VLY53LK&pd_rd_w=1TwO3&pf_rd_p=c1d7adc4-342a-461c-887e-847da81a421c&pd_rd_wg=GTknw&pf_rd_r=Z49NKW6WNDXCTXYFHJ8A&pd_rd_r=0ecd63d6-5bc0-4cf5-9d0d-2f3fc6f7bef1&smid=A38BYO16F77RDO&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSTVKSEpKOUQwVUxHJmVuY3J5cHRlZElkPUEwMTEwNjMwMkhJSkZYTjhJM0pHMSZlbmNyeXB0ZWRBZElkPUExQTFUTEtBQ1Q2RzRDJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==" style="color: black;"> Click here </a> </p>
+  </div>
+</div>
+
+
+<div class="column">
+  <div class="card">
+    <img src="style/44.jpg" alt="cat" id="imgCard" >
+    <h3>Cat Glasses </h3>
+  <p> <a href="https://www.amazon.com.au/Glasses-Sunglasses-Eye-wear-Accessories-Products/dp/B088LZDC7D/ref=sr_1_38?crid=2YH1257OKGN2F&dchild=1&keywords=cat+accessories&qid=1606496758&s=pet-supplies&sprefix=cat+ac%2Cpets%2C414&sr=1-38" style="color: black;"> Click here </a> </p>
+  </div>
+</div>
+
+
+<div class="column">
+  <div class="card">
+    <img src="style/47.jpg" alt="cat" id="imgCard" >
+    <h3> Cat Litter Box  </h3>
+  <p> <a href="https://www.amazon.com.au/Bloodyrippa-Litter-Removable-Prevent-Scatter/dp/B07QV4W6XM/ref=sr_1_7?crid=7QYQHZPYSW8N&dchild=1&keywords=cat+litter+box&qid=1606498234&s=pet-supplies&sprefix=li%2Cpets%2C514&sr=1-7" style="color: black;"> Click here </a> </p>
+  </div>
+</div>
+
+
+<div class="column">
+  <div class="card">
+    <img src="style/32.jpg" alt="cat" id="imgCard" >
+    <h3> Brush </h3>
+  <p> <a href="https://www.amazon.com.au/Pin-Brush-Hertzko-Detangling-Undercoat/dp/B01IDZAEC2/ref=sr_1_59?dchild=1&keywords=cat+brush&qid=1606497670&s=pet-supplies&sr=1-59" style="color: black;"> Click here </a> </p>
+  </div>
+</div>
+
+
+<div class="column">
+  <div class="card">
+    <img src="style/17.jpg" alt="cat" id="imgCard" >
+      <h3> Cat Collar </h3>
+    <p> <a href="https://www.amazon.com.au/Ozpaw-Padded-Leather-Collar-Puppies/dp/B07TV484QK/ref=sr_1_6?dchild=1&keywords=cat+collar&qid=1606496351&sr=8-6" style="color: black;"> Click here </a> </p>
+  </div>
+</div>
+
+</div>
+
+      </div>
+  <br>
+  <br>
+  <hr>
+  <br>
+
+<?php require_once 'style/footer.php';?>
