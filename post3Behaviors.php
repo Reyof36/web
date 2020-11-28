04f63bccd9c265613afe09068b6ef8b8f40723e6
Samar-Alhariqi <75090108+Samar-Alhariqi@users.noreply.github.com>
2020-11-28 05:56:35 +0300

    Update post3Behaviors.php

--- post3Behaviors.php
+++ post3Behaviors.php
@@ -1,6 +1,6 @@
 <?php require_once 'style/header.php';?>
 
-<title> Pet Cat | How To Stop My Cat From Bullying My Other Cat? </title>
+<title> Pet Cat | Why Does My Cat Meow At Night? </title>
 <br>
 <br>
 <hr>
@@ -26,23 +26,115 @@
     }
 
 </style>
+
 <div class="container-flex">
   <!--Add breedcrumb-->
   <ul class="breedcrumb">
     <li> <a href="index.php"> Home </a> </li>
     <li> <a href="Behaviors.php"> Behaviors</a> </li>
-    <li> How To Stop My Cat From Bullying My Other Cat? </li>
+    <li> Why Does My Cat Meow At Night? </li>
   </ul>
 
   <br>
   <br>
-   <h2> </h2>
+   <h2>Why Does My Cat Meow At Night? </h2>
+   <img src="style/CatMeow2.jpg" alt="Cat Meow" width="700" height="400">
+   <p>
+     <p>There are several reasons as to why a cat meows, but what proves to be puzzling to most cat parents is
+        why do they choose to do so at night?<br>
+     <br>Now, contrary to popular belief, cats are not lazy animals.
+         Just because they sleep for the better part of the day does not mean that they are lazy.
+         Instead, they save their energy for night-time as they are nocturnal.
+         They instinctively sleep a day and play and hunt all night.<br>
+     <br>What is causing you lack of sleep, what you refer to as meowing is called night vocalization or night calling.
+         Cats can make a wide range of noises; they can spit, hiss, yowl, purr and meow. If you are keen enough,
+         you will notice that cats only meow at humans. Your cat knows that you do not speak its language and
+         as such reserve the meowing to you. So the question is what is your furry feline trying to tell you?<br>
+     <br>Before we get to why cat’s meow at night, let us first understand why they meow during the day and in general. </p> <br>
 
+     <h4> Hungry </h4>
+     <p> Your cat may be meowing in order to communicate that it is hungry and that you need to
+         feed it with some food (preferably some high protein cat food). Mind you, cats are picky animals.
+         It might have some kibble on its bowl but it will continue fussing until you fill it up. </p>
 
+       <br>
+       <h4> Thirsty </h4>
+       <p> Yes, cats need water too. For the longest time. Back to your cats thirst. Whether your cat’s bowl has run out of water
+           or it has an object floating on it, your cat will make noises to make you aware of it.
+           If you want to ensure your cat never gets thirsty, and that he/she is occupied most times when bored,
+           you should consider investing in a cat water fountain.</p>
 
+       <br>
+       <h4> Clean the Cat Litter Box </h4>
+       <p> Cats are great at cleaning themselves with their tongues. So much so that regular and frequent cleaning is not necessary.
+           With their level of cleanliness, it should not come as a shock to you when your cat needs her/his litter box spotless.</p>
 
+       <br>
+       <h4> I Want To Go Outside I Want to Come Inside </h4>
+       <p> When dogs want to go outside, they stand on their hind legs and scratch at the front door. This is a clear indication that it wants out.
+           While cats may not do this, they communicate their need to be outside by standing at the door and meowing.</p>
 
+       <br>
+       <h4> Attention </h4>
+       <p> Cats that are left in the house alone for prolonged periods often meow at the owners for some play time or just simple petting.
+         Though independent animals, cats also need the love and care from their adults as well.</p>
 
+       <br>
+       <h4> Looking for a Mate </h4>
+       <p> If your cat has not been neutered or spayed, it still has its reproductive organs intact and fully functional. With all the noise
+           it is making, your cat may be in need of a mate. The type of meow made during this time is
+           however slightly different from the rest. It also has a different name – yowling.<br>
+       <br> Male cats will yowl to find a female that is interested in them while female cats will yowl to signal male cats that it is on heat.</p>
+
+       <br>
+       <h4> It is Feeling Unwell </h4>
+       <p> Like human babies, cats cannot communicate when they are feeling unwell.
+           They cannot tell you exactly where it hurts or generally how they are feeling. This leaves you the cat parent confused.
+           Your best bet of diagnosing the problem is visiting a professional vet.<br>
+       <br> That said, when your cat is feeling unwell, it will give you some signals including being restless,
+            being hungry and thirsty always and being irritable.<br>
+       <br> Why does my cat meow at night?<br>
+       <br> In addition to the above reasons, here is why your cat is meowing at night.</p>
+
+       <br>
+       <h4> Senior Cats </h4>
+       <p> Like old humans, old cats also have some special needs. They require more attention compared to younger cats.
+           Additionally, elderly cats tend to meow during the night a lot as they seek help.
+           Some of the common health problems including arthritis can cause discomfort and impair your cat’s daily activities.
+           Getting itself to the litter, to the water fountain and food bowl may prove to be a huge and painful task for an elderly cat. </p>
+
+       <br>
+       <h4> CDS </h4>
+       <p> CDS stands for Cognitive dysfunction syndrome in full. It is the cat’s version of the human Alzheimer’s disease.
+           As your cat grows older, it may start getting confused at night.
+           It meows as it is distressed and is looking for a safe place to spend the night. </p>
+
+       <br>
+       <h4> It is Stuck and Needs Some Help </h4>
+       <p> Cats are flexible. They always land on their feet. I find this all too amazing. Nonetheless, from time to time, cats get stuck as well.
+           Your cat may have gotten itself in some trouble. It probably is stuck in a locked room, in a clothes hamper or is stuck in a closet.
+           Getting stuck causes panic to set in and your cat to make the meowing sounds.<br>
+       <br>Now that you know the answer to, ‘why does my cat meow at night?’ how can you possibly stop
+           it and at least get some rest at night?</p>
+
+       <br>
+       <h4> How to Stop Your Cat From Making Noises </h4>
+       <p> If your cat keeps you awake all night and you have had enough of it, there are some methods you can
+           apply to get it to stop meowing or meow less. The methods require patience and consistency but in the end are worth it.<br>
+       <br> The first step to finding the remedy is to determine why your cat is meowing at night.
+            It may not be obvious at first and you probably might want to visit the vet just in case there is a serious underlying problem.<br>
+       <br> If your cat needs food, ensure that there is always food for it. You can get the best automatic cat feeder
+            or alternatively, you can train your cat to feed at specific times of the day.<br>
+       <br> If your cat is thirsty, ensure that it has fresh water always. In line with this,
+            ensure that the bowl is clean. If you cannot drink from the bowl, there are high chances that your cat will not either.<br>
+       <br> If your cat is making noises because of a dirty litter box, ensure that the cleaning is done frequently and often.
+            The frequency of the cleaning depends on the number of cats you have and how many times your cat poops in a day.
+            While cleaning the litter box, be sure to return it in the exact spot you got it failure to do so will result in your cat meowing all the more.<br>
+       <br> If it is a cry for attention try and play with it a while (30 minutes will do).
+            Snuggling will also work wonders on your cat.</p>
+
+        <br>
+  </p>
   <br>
   <br>
   <!--add comment form-->
@@ -53,18 +145,7 @@
   </div>
   <br>
   <br>
-
 </div>
-
-
-
-
-
-
-
-
-
-
 
 <br>
 <br>
