14040b4bab5082bcabbd520e10a524cab527aeff
Samar-Alhariqi <75090108+Samar-Alhariqi@users.noreply.github.com>
2020-11-28 05:59:58 +0300

    Add files via upload

---
+++ post4Behaviors.php
@@ -0,0 +1,117 @@
+<?php require_once 'style/header.php';?>
+
+<title> Pet Cat | Why Does My Cat Scratch The Wall? </title>
+<br>
+<br>
+<hr>
+<br>
+<br>
+<style>
+  .container-flex h2{
+    margin-left:40px;
+  }
+  .container-flex img{
+    margin: 40px;
+    margin-top: 15px;
+  }
+  .container-flex p {
+    margin-left: 40px;
+    margin-right: 30px;
+  }
+  .container-flex h4{
+    margin-left: 50px;
+  }
+  .container-flex ul {
+      margin-left: 50px;
+    }
+
+</style>
+
+<div class="container-flex">
+  <!--Add breedcrumb-->
+  <ul class="breedcrumb">
+    <li> <a href="index.php"> Home </a> </li>
+    <li> <a href="Behaviors.php"> Behaviors</a> </li>
+    <li> Why Does My Cat Scratch The Wall? </li>
+  </ul>
+
+  <br>
+  <br>
+  <h2>Why Does My Cat Scratch The Wall? </h2>
+  <img src="style/CatScratch2.jpg" alt="Cat Scratch The Wall" width="700" height="400">
+  <p>
+      <p>It is actually pretty normal for a cat to scratch on things: furniture, pillows, walls;
+         almost anything! But what exactly is the reason behind this habit of theirs? Is there any way
+         to keep your cats off the furniture, or the floor, or the wall, or the mats? Or primarily from scratching?<br>
+      <br>Well, read along and let’s claw our way in to find out why does a cat scratch the wall! </p> <br>
+
+      <h4> So Why Do They Scratch Walls? </h4>
+      <p> There are a number of reasons as to why your cat will scratch your wall – or well, pretty much any surface.
+          Basically, they do so for their benefit, and at the expense of your furniture.
+          But I guess that’s the thing with cats, and when we decided to have them as our fur babies;
+          the best thing we can do now is to learn more about this particular habit of theirs and understand them better. </p>
+
+      <br>
+      <h4> Your wall turned "whetstone" </h4>
+      <p> For your cat, it does not see your wall as merely being just a wall. It has turned into a whetstone and a gym in one.
+          Very similar to how a whetstone functions, a cat scratching on surfaces wants to keep its claws sharp. You see surfaces,
+          and conveniently your wall, once scratched will allow your cat to shed off old layers of claws to give way to new, way sharper claws. </p>
+
+      <br>
+      <h4> A message to other cats </h4>
+      <p> As much as they are sharpening their claws when they scratch the wall, at the same time they are also able to leave "scent messages"
+          of these surfaces. These scent messages aren't for us humans, as we won't even be able to notice it, it is in fact for other cats if you have others at home.
+          It is their way of communicating with other cats. </p>
+
+     <br>
+     <h4> Cat was here </h4>
+     <p> Cats are highly territorial, very much like other animals. They have a number of ways to
+         leave a mark on certain parts of your home and you may not even know it. You might have wondered why some cats pee on your stuff and such,
+         well to tell you the truth they’re simply marking the territory that belongs to them.<br>
+     <br> In this case, it will be really hard to miss as you will see the aftermath of a cat just had scratched off the wall.
+          Sometimes you would even have your cat caught in the act. </p>
+
+     <br>
+     <h4> Stress relief </h4>
+     <p>  Your cat could also be feeling a bit stressed or anxious. It is a possible reason why a cat would scratch the wall - scratching the walls could
+          be their way of releasing some of the tension and relieving a bit of that stress. You can check for other signs and symptoms if this is really the case.</p>
+
+     <br>
+     <h4> Diversionary tactics </h4>
+     <p> Perhaps your wall has a really nice texture that your cat has taken a liking to.
+         You can prevent your cat from scratching off walls by giving it its own personal scratcher.
+         Perhaps it is high time you should get your cat its very own personal scratch post!
+         There are a lot of cat scratch post out there that could satisfy your cat’s intense scratching tendencies.</p>
+
+     <br>
+     <h4> Repel </h4>
+     <p> Most cats do not like certain scents. What you can do is find out which scent deters your feline and use that scent to ward it off the wall.
+         Citrus and lemony scents work most of the time. You can also use the same effect by finding out which scents
+         get the attention of your cats and spray it on that scratch post you got your cat.</p>
+
+     <br>
+     <h4> Declawing </h4>
+     <p> If the first two attempts did not work for you, you can opt to declaw your cat. While most cat parents would not recommend doing so,
+         due to the fact that it could have psychological effects on your cat; only choose this option if there is really nothing that seems to be working.</p>
+
+       <br>
+   </p>
+   <br>
+   <br>
+   <!--add comment form-->
+   <div class="comment">
+    <h3 style="text-align:center; color:gray;"> Leave a comment </h3>
+    <br>
+       <?php require_once 'comment.php';?>
+   </div>
+   <br>
+   <br>
+ </div>
+
+ <br>
+ <br>
+ <hr>
+ <br>
+ <br>
+
+ <?php require_once 'style/footer.php';?>
