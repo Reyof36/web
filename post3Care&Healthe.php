02b7691fc0ea48ef2380c87113dbdfa9db0b896a
Esraa <75171813+eman-2@users.noreply.github.com>
2020-11-28 17:42:14 +0300

    Add files via upload

---
+++ post3Care&Healthe.php
@@ -0,0 +1,123 @@
+<?php require_once 'style/header.php';?>
+<title> Pet Cat | How to take care of a cat when it is sick ? </title>
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<style>
+  .container-flex h2{
+    margin-left:40px;
+  }
+  .container-flex img{
+    margin: 40px;
+    margin-top: 15px;
+  }
+  .container-flex p {
+    margin-left: 40px;
+    margin-right: 30px;
+  }
+  .container-flex h4{
+    margin-left: 50px;
+  }
+  .container-flex ul {
+      margin-left: 50px;
+      margin-right: 30px;
+    }
+</style>
+
+<div class="container-flex">
+  <!--Add breedcrumb-->
+  <ul class="breedcrumb">
+    <li> <a href="index.php"> Home </a> </li>
+    <li> <a href="care&health.php"> Care&Health</a> </li>
+    <li> How to take care of a cat when it is sick </li>
+  </ul>
+
+  <br>
+  <br>
+  <h2> How to take care of a cat when it is sick </h2>
+  <img src="style/sick cat.jpg" alt="Sick Cat" width="700" height="400">
+
+  <p>
+    <h4>How do you recognise a sick or injured cat?</h4>
+    <p> To recognise when a cat is sick or in pain, you need to know what a healthy, happy cat looks like. 
+      A healthy cat is active, with clear, bright eyes, a good appetite, is pooping and peeing normally, has smooth fur and clean skin.
+       A happy cat moves with comfort and grace, and breathes without any strange noises.
+        If there is a sudden change in one or several of these areas, it may be a sign that the cat is ill or in pain.
+         Read more about the symptoms of a sick cat here.<br>
+   <br> If you recognise symptoms of illness or pain, consult your vet.
+    But what should you do before you go to the vet? In case of accident or illness:</p>
+
+    <br>
+    <h4>First aid after an accident</h4>
+    <p>There is no specific time that is ideal for giving your cat a shower. However, it is better to wash
+       the cat when the weather is warm. It makes it easier for the cat to dry. Other than that, there are
+       no other preferences on when to wash the cat. If you have a blow dryer, even washing it a night and
+       using the dryer (which might scare the cat initially) to dry the cat can work.
+      <ul>
+        <li>Limping, difficulty moving.</li>
+        <li>Pain when touched or petted</li>
+        <li>Wounds: swelling or fluid in the affected area, bleeding.</li>
+        <li>Difficulty in breathing, cardiac arrest</li>
+  </ul>  </p>
+
+
+    <br>
+    <h4>First aid in case of illness</h4>
+    <p>Your cat might feel a bit under the weather at some point and not all symptoms are a reason to worry too much. 
+      For example, throwing up doesn't have to be a symptom of a serious condition at all: it can just be a hairball or indigestion. 
+      In order to recognize the more serious symptoms, observe your cat when he or she is in a healthy cat state so that you can spot a change in behaviour or appearance. 
+      Are you a cat sitter with a reservation coming up? Make sure you arrange a thorough meet & greet beforehand so that you know how the cat behaves normally.
+       Ask the owner to tell you everything there is to know about the cat so you're already in the loop.<br>
+   <br>Very common symptoms in case of illness are: <br>
+    <ul>
+      <li>Difficulty breathing.</li>
+      <li>Coughing up phlegm.</li>
+      <li>Constantly scratching and licking.</li>
+      <li>Difficulty urinating / little urination.</li>
+      <li>Sudden lack of self-cleaning or smelliness.</li>
+      <li>Sudden drowsiness, lethargy or paralysis.</li>
+      <li>Weight loss or sudden weight gain.<li>
+    </ul>
+  <br>These symptoms can indicate many conditions and it is important to call the vet if they show up. In the meantime: provide cat care by:</br>
+  <ul>
+    <li>Staying calm.</li>
+    <li>SMaking sure to keep the cat a comfortable temperature: provide some blankets if it's cool, or shade if it's hot.</li>
+    <li>Providing a quiet, dark place without other pets or children around.</li>
+    <li>Making sure there is sufficient water available, especially in case of diarrhoea. If the cat doesn't drink, provide wet food.</li>
+    <li>Clean your cat's dirty nose, eyes or backside with clean, damp cotton wool.</li>
+  </ul>
+</p>
+
+    <br>
+    <h4>What should I as a pet sitter do in case of emergency?</h4>
+    <p>If you are a pet sitter, it might happen that a cat or dog gets sick or injured while under your care. 
+      Therefore, good preparation is essential. Make sure to ask for the pet's medical information and the contact details of the vet beforehand.
+       Make sure you have a travel carrier handy and a first aid kit. In that, you will find things such as clean bandages, cotton wool, saline solution, ointment for wounds and a thermometer.
+    <br>If you observe any symptoms of illness in the cat or spot any signs that it's injured, always inform the pet owner, for example via the Pawshake app. Please make sure not to panic or cause the owner too much worry.
+     Just calmly tell them what you have observed and share your concerns. Maybe the owner can provide you with some useful information because they know their cat best. Explain to the owner what your next steps will be.</br>
+     <br>Can't reach the owner? Make sure to take action anyway - the safety and health of the pet is always, always the top priority. Provide first aid and call the vet. 
+     With every Pawshake booking, the pet is covered in the event of sudden illness or accidents, so always ask the vet for a receipt. </br>
+  <br>
+  </p>
+  <br>
+  <br>
+  <!--add comment form-->
+  <div class="comment">
+   <h3 style="text-align:center; color:gray;"> Leave a comment </h3>
+   <br>
+      <?php require_once 'comment.php';?>
+  </div>
+  <br>
+  <br>
+</div>
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<?php require_once 'style/footer.php';?>
